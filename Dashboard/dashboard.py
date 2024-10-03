30534ce3833fe982a8b98012ac1924a1a02e426f
notariq <hauzanbakri@example.com>
2024-10-03 13:29:45 +0700

    add data fix dashboard

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -5,44 +5,12 @@ import matplotlib.pyplot as plt
 
 # Load
 def load_data():
-    orders_df = pd.read_csv('Dashboard/orders_dataset.csv')
-    order_reviews_df = pd.read_csv('Dashboard/order_reviews_dataset.csv')
-    
-    return orders_df, order_reviews_df
-
-# Processing
-def clean_data(orders_df, order_reviews_df):
-    success_orders_df = orders_df[orders_df['order_status'] == 'delivered'].dropna()
-    order_reviews_df['review_comment_title'] = order_reviews_df['review_comment_title'].fillna('No Title')
-    order_reviews_df['review_comment_message'] = order_reviews_df['review_comment_message'].fillna('No Message')
-
-    # Convert date columns to datetime
-    date_columns = [
-        'order_delivered_customer_date',
-        'order_approved_at',
-        'order_delivered_carrier_date',
-        'order_purchase_timestamp',
-        'order_estimated_delivery_date'
-    ]
-    for col in date_columns:
-        success_orders_df[col] = pd.to_datetime(success_orders_df[col], errors='coerce')
-
-    # Create status column
-    success_orders_df['status'] = success_orders_df.apply(
-        lambda row: 'Early' if row['order_delivered_customer_date'].date() < row['order_estimated_delivery_date'].date() else
-                     'Late' if row['order_delivered_customer_date'].date() > row['order_estimated_delivery_date'].date() else
-                     'On Time', axis=1
-    )
-    success_orders_df['delivery_time_days'] = (success_orders_df['order_delivered_customer_date'] - success_orders_df['order_purchase_timestamp']).dt.days
-
-    # Merge dataframes
-    review_df = pd.merge(success_orders_df, order_reviews_df, how="left", on="order_id")
-    
-    return success_orders_df, review_df
+    review_df = pd.read_csv('Dashboard/all_data.csv')
+    return review_df
 
 # Visualization
-def plot_status_counts(success_orders_df):
-    status_counts = success_orders_df['status'].value_counts()[['Early', 'On Time', 'Late']]
+def plot_status_counts(review_df):
+    status_counts = review_df['status'].value_counts()[['Early', 'On Time', 'Late']]
     plt.figure(figsize=(10, 6))
     plt.bar(status_counts.index, status_counts.values, color=['#0d62ff', '#5792ff', '#8ab3ff'])
     plt.title('Status Pesanan', fontsize=14)
@@ -52,11 +20,11 @@ def plot_status_counts(success_orders_df):
         plt.text(i, value, value, ha='center', va='bottom')
     st.pyplot(plt)
 
-def plot_delivery_time_distribution(success_orders_df):
+def plot_delivery_time_distribution(review_df):
     plt.figure(figsize=(10, 6))
-    plt.hist(success_orders_df['delivery_time_days'], bins=30, color='#5792ff', alpha=0.6, edgecolor='black')
-    plt.axvline(success_orders_df['delivery_time_days'].mean(), color='red', linestyle='dashed', linewidth=1, label='Mean')
-    plt.axvline(success_orders_df['delivery_time_days'].median(), color='green', linestyle='dashed', linewidth=1, label='Median')
+    plt.hist(review_df['delivery_time_days'], bins=30, color='#5792ff', alpha=0.6, edgecolor='black')
+    plt.axvline(review_df['delivery_time_days'].mean(), color='red', linestyle='dashed', linewidth=1, label='Mean')
+    plt.axvline(review_df['delivery_time_days'].median(), color='green', linestyle='dashed', linewidth=1, label='Median')
     plt.xlabel('Waktu Pengiriman (Hari)', fontsize=12)
     plt.ylabel('Frekuensi', fontsize=12)
     plt.title('Distribusi Waktu Pengiriman', fontsize=16)
@@ -80,15 +48,11 @@ def main():
     st.title("Analisa Pengiriman dan Kepuasan Pelanggan ✨")
     st.caption("| Hauzan Ariq Bakri | Oktober 2024 |")
     st.write("")
-    orders_df, order_reviews_df = load_data()
-    success_orders_df, review_df = clean_data(orders_df, order_reviews_df)
+    review_df = load_data()
 
     # Dashboard Sections
-    if st.checkbox('Data Pesanan'):
-        st.write(orders_df.sample(5))
-
-    if st.checkbox('Data Ulasan'):
-        st.write(order_reviews_df.sample(5))
+    if st.checkbox('Tampilkan Data'):
+        st.write(review_df.sample(5))
 
     # 1
     st.subheader("Status Pesanan")
@@ -97,8 +61,8 @@ def main():
         Banyak pesanan yang sampai lebih awal dari perkiraan estimasi perjalanan dengan nilai **88630**, dilanjutkan dengan pesanan telat yang berjumlah cukup banyak yaitu **6533**, dan pesanan yang sampai tepat waktu sebanyak **1292**.
         """
     )
-    st.write(success_orders_df.groupby(by="status").order_id.nunique().sort_values(ascending=False))
-    plot_status_counts(success_orders_df)
+    st.write(review_df.groupby(by="status").order_id.nunique().sort_values(ascending=False))
+    plot_status_counts(review_df)
 
     # 2
     st.subheader("Waktu Pengiriman")
@@ -107,19 +71,19 @@ def main():
         Selanjutnya kita akan melihat berapa lama sebuah pesanan sampai ke tujuan (dari pelanggan membayar pesanan), kita akan menghitung selisih dari *order_delivered_customer_date* (tanggal sampai) dan *order_purchase_timestamp* (tanggal pembelian), nantinya hasil akan dimasukkan ke kolom *delivery_time_day*
         """
     )
-    st.write(success_orders_df['delivery_time_days'].describe())
+    st.write(review_df['delivery_time_days'].describe())
     st.markdown(
         """
         Dari rangkuman statistik tersebut kita dapat melihat beberapa poin penting, dari 96455 data, pesanan memiliki rata-rata waktu **12 hari** perjalanan dengan pesanan yang paling cepat adalah **dihari yang sama (0 hari)** dan pesanan yang paling lama sampai bernilai **209 hari**.
         """
     )
-    plot_delivery_time_distribution(success_orders_df)
+    plot_delivery_time_distribution(review_df)
 
     # 3
     st.subheader("Hubungan Pengiriman dengan Kepuasan Pelanggan")
     st.markdown(
         """
-        Melakukan analisa untuk melihat hubungan atnara kecepatan pengiriman dengan skor ulasan, pertama-tama kita akan lakukan pengecekkan korelasi antara kolom *delivery_time_days* dengan *review_score*.
+        Melakukan analisa untuk melihat hubungan antara kecepatan pengiriman dengan skor ulasan, pertama-tama kita akan lakukan pengecekkan korelasi antara kolom *delivery_time_days* dengan *review_score*.
         """
     )
     
@@ -127,20 +91,20 @@ def main():
     st.write(correlation)
     st.markdown(
         """
-        Ternyata lamanya pengiriman pesanan ***Berkolerasi Negatif Sedang*** dengan skor ulasan, dengan begitu menunjukkan bahwasannya ketika nilai  *delivery_time_days* naik, maka nilai *review_score* cenderung menurun, begitu pula sebaliknya.
+        Ternyata lamanya pengiriman pesanan ***Berkorelasi Negatif Sedang*** dengan skor ulasan, dengan begitu menunjukkan bahwasannya ketika nilai *delivery_time_days* naik, maka nilai *review_score* cenderung menurun, begitu pula sebaliknya.
         """
     )
     
     st.markdown(
         """
-        Kita dapat membuat kategori rentang (***bins***) hari perjalanan pesanan dengan menggunakan fungsi ***cut()*** milik pandas, kita akan mendefinisikan rentang sebanyak 5 atau 10 interval nantinya data kategori akan dimasukkan ke kolom *delivery_time_category*. Kemudian kategori tersebut akan di tampilkan menggunakan pivot table beserta dengan rata-rata skor ulasan.
+        Kita dapat membuat kategori rentang (***bins***) hari perjalanan pesanan dengan menggunakan fungsi ***cut()*** milik pandas, kita akan mendefinisikan rentang sebanyak 5 atau 10 interval nantinya data kategori akan dimasukkan ke kolom *delivery_time_category*. Kemudian kategori tersebut akan ditampilkan menggunakan pivot table beserta dengan rata-rata skor ulasan.
         """
     )
-    review_df['delivery_time_category'] = pd.cut(
-        review_df['delivery_time_days'],
-        bins=[0, 5, 10, 15, 20, 30, 40, 100],
-        labels=['0-5', '6-10', '11-15', '16-20', '21-30', '31-40', '40+']
-    )
+    #review_df['delivery_time_category'] = pd.cut(
+    #    review_df['delivery_time_days'],
+    #    bins=[0, 5, 10, 15, 20, 30, 40, 100],
+    #    labels=['0-5', '6-10', '11-15', '16-20', '21-30', '31-40', '40+']
+    #)
     st.write(review_df.groupby('delivery_time_category').agg({'review_score': 'mean', 'order_id': 'count'}).reset_index())
     st.markdown(
         """
